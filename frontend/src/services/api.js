8f88a3d412b464b68f7b3d02237081b1804b0b56
[200~dhanushprathipati2007-create~ <dhanushprathipati2007@gmail.com>
2026-07-16 14:28:54 +0530

    Connect frontend to Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ai-code-review-1xi9.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
